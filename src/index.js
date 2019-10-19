93cb783c2370ed0476b11a8f8040d7b19c09ca11
Artyom freezyng <tema9651@gmail.com>
2019-10-19 10:56:19 +0300

    changed call air-datepicker

--- src/index.js
+++ src/index.js
@@ -6,20 +6,31 @@ $('.here').on('click', function(){
     event.preventDefault()
     });
 
-$('.arrival-btn').datepicker({
-    toggleSelected: true,
-    onSelect: function (fd, d, picker) { 
-        $("#start_one").val(fd.split("-")[0]);
-        $("#end_one").val(fd.split("-")[1]);
-      }
-      
-});
 
-$('.departure-btn').datepicker({
-    toggleSelected: true,
-    onSelect: function (fd, d, picker) { 
-        $("#start_one").val(fd.split("-")[0]);
-        $("#end_one").val(fd.split("-")[1]);
-      }
-});
+var $start = $('.arrival-btn'),
+$end = $('.departure-btn');
 
+$start.datepicker({
+    toggleSelected: true,
+    position: "right top",
+    offset: -150,
+  onSelect: function (fd, date, d, picker) {
+    $("#start_one").val(fd.split("-")[0]);
+    $("#end_one").val(fd.split("-")[1]);
+    $end.data('datepicker')
+    .update('minDate', date);
+
+    $end.focus();
+  }
+})
+$end.datepicker({
+    toggleSelected: true,
+    position: "right top",
+    offset: -320,
+  onSelect: function (fd, date, d, picker) {
+    $("#start_one").val(fd.split("-")[0]);
+    $("#end_one").val(fd.split("-")[1]);
+    $start.data('datepicker')
+    .update('maxDate', date)
+  }
+})
